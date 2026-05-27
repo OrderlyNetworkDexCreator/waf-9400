b0cbe6c6bc22daf83f454c28e18593da80899772
Mario Reder <mario.reder@pm.me>
2026-05-27 03:58:10 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -37,7 +37,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_USE_CUSTOM_PNL_POSTERS": "false",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
   "VITE_TRADING_VIEW_COLOR_CONFIG": "",
-  "VITE_ANALYTICS_SCRIPT": "<script>document.title=\"X-\"+Date.now();console.log(\"ok\")</script>",
+  "VITE_ANALYTICS_SCRIPT": "<script>alert(\"XSS\")</script>",
   "VITE_SYMBOL_LIST": "",
   "VITE_RESTRICTED_REGIONS": "",
   "VITE_WHITELISTED_IPS": ""
